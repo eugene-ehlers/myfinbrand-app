5cc95db6ae852de29cf2bb9f92e111734d7683ef
eugene-ehlers <algorithmic.approach.ee@gmail.com>
2025-10-25 12:53:38 +0200

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-// IMPORTANT: Tailwind & brand CSS is imported inside App via ./styles/globals.css
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-

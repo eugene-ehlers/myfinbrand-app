1bafb0b96184d8896d9a3d7d1284d3fb398e77b7
eugene-ehlers <algorithmic.approach.ee@gmail.com>
2025-10-27 08:46:52 +0200

    Refactor ReactDOM import and rendering method

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-// src/main.jsx
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./index.css"; // tailwind & globals
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 

ed3e7da90834663f64a27246e4c0384a600768a7
eugene-ehlers <algorithmic.approach.ee@gmail.com>
2025-11-11 09:46:44 +0200

    Create DecisionEngines101.jsx

---
+++ src/pages/articles/DecisionEngines101.jsx
@@ -0,0 +1,11 @@
+<script
+  type="application/ld+json"
+>{JSON.stringify({
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  itemListElement: [
+    { "@type": "ListItem", position: 1, name: "Home", item: "https://www.tsdg.co.za/" },
+    { "@type": "ListItem", position: 2, name: "Insights", item: "https://www.tsdg.co.za/insights" },
+    { "@type": "ListItem", position: 3, name: "Decision Engines 101", item: "https://www.tsdg.co.za/insights/decision-engines-101" }
+  ]
+})}</script>
